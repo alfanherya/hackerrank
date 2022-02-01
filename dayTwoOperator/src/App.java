8bb6356afc41932a1c06e4d18d770b8160e49eec
adl.alfanh@xl.co.id <adl.alfanh@xl.co.id>
2022-02-01 16:35:31 +0700

    30 D Operator challange

---
+++ dayTwoOperator/src/App.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class App {
+
+    // complete the solve function below
+    static void  solve(double meal_cost, int tip_percent, int tax_percent){
+        double tip = meal_cost * tip_percent/100;
+        double tax = meal_cost * tax_percent/100;
+        double totalCost =  Math.round(meal_cost + tip + tax);
+        int value = (int) totalCost;
+        System.out.println("The total meal cost is " + value+ " dollars.");
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+    
+    public static void main(String[] args) throws Exception {
+        double meal_cost = scanner.nextDouble();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tip_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])");
+
+        int tax_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+
+    }
+
+    
+
