ee1b28412bcfb7081c9a427f71db242e2992e95d
adl.alfanh@xl.co.id <adl.alfanh@xl.co.id>
2022-02-01 12:23:00 +0700

    challange 30D data type

---
+++ challange/src/App.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        // Declare second integer, double, and String variables.
+        int i2;
+        double d2;
+        String s2;
+
+        // Read and save
+        i2 = scan.nextInt();
+        d2 = scan.nextDouble();
+        scan.nextLine();
+        s2 = scan.nextLine();
+
+        // print the sum of both integer variables on a new line.
+        System.out.println(i + i2);
+
+        // print the sum of the double variables on a new line.
+        System.out.println(d + d2);
+
+        // Concatenate and print the String variables on a new line
+        // the 's' variable above should be printed first.
+        System.out.println(s + s2);
+        scan.close();
+    }
+}
