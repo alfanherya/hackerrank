eab945f7f7aad302e2cffcbf3a03b1f45270ed48
adl.alfanh@xl.co.id <adl.alfanh@xl.co.id>
2022-02-15 22:01:34 +0700

    day 11 - 2D array

--- dayThree/src/App.java
+++ dayThree/src/App.java
@@ -4,15 +4,29 @@ import java.io.InputStreamReader;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-       int N = Integer.parseInt(bufferedReader.readLine().trim());
-        
-       if(N % 2 == 0){
-           if(N < 3) System.out.println("Not Weired");
-           else if(N < 24) System.out.println("Weird");
-           else System.out.println("Not Weired");
-       } else System.out.println("Weird");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
 
-       bufferedReader.close();
+        if (N % 5 == 0) {
+            System.out.println("A");
+        }
+        if (N % 5 == 1) {
+            System.out.println("B");
+        }
+        if (N % 5 == 2) {
+            System.out.println("C");
+        }
+        if (N % 5 == 3) {
+            System.out.println("D");
+        }
+        if (N % 5 == 4) {
+            System.out.println("E");
+        } else {
+            System.out.println("Nothing");
+            // 0=a, 1=b, 2=c, 3=d, 4=e
+
+            bufferedReader.close();
+        }
     }
+
 }
